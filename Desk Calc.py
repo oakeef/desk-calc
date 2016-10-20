d040e2701b767526c5a7bdbf962e0fe6834de415
Evan <oakeef@gmail.com>
2016-10-20 16:34:27 -0300

    Added a few comments and spaced out code
    Comments were added to clear up what variables were for and I spaced out
    the code a bit to make it easier to read. Modified the README with some
    plans I have for the future of this little Python program.

--- Desk Calc.py
+++ Desk Calc.py
@@ -1,13 +1,25 @@
-__author__ = 'w0174181'
+__author__ = 'Evan FO'
+
 #the charge for all desks is minimum $200
 minimum = 200
+
+#If a desk has a surface area of 750 square inches of above 
+#than it is considered a large desk and the cost increases by $50
 largeSurfaceSize = 750
 largeSurfaceCost = 50
+
+#If the desk is made of Mahogany than there is a $150 premium added
 mahoganyCost = 150
+#If it's Oak than there is a $125 premium added
 oakCost = 125
+#Pine is no additional cost
 pineCost = 0
+
+#If the customer wants drawers they are $30 each
 drawerCost = 30
-#input
+
+
+#input section
 #Enter order number:
 ordernumber = input("Enter order number: ")
 #Enter length of desk (inches):
@@ -18,6 +30,8 @@ width = float(input("Enter width of desk (inches): "))
 woodType = str(input("Enter type of wood (mahogany, oak, pine):"))
 #Enter number of drawers:
 drawers = int(input("Enter number of drawers: "))
+
+
 #math time
 #set total as minimum amount it will possibly be
 total = minimum
@@ -34,9 +48,11 @@ if woodType.lower() == "oak":
 
 if woodType.lower() == "pine":
     total += pineCost
-#for every drawer in the desk, ther is an additional $30 charge
+#for every drawer in the desk, there is an additional $30 charge
 totalDrawerCost = drawers * drawerCost
 total += totalDrawerCost
+
+
 #output
 #total cost
 print("Total cost is : ${0:.2f}".format(total))
